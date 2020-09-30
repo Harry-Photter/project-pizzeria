6627804c1b28ef9394cde38f69c2fddf85d5d922
Mateusz Furman <mateuszfurman.nf@gmail.com>
2020-09-30 14:04:50 +0200

    Instance for each product

--- src/js/script.js
+++ src/js/script.js
@@ -62,8 +62,19 @@
 
   const app = {
     initMenu: function () {
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+  
+      thisApp.data = dataSource;
     },
 
     init: function () {
@@ -74,10 +85,10 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
-
 
   app.init();
 }
